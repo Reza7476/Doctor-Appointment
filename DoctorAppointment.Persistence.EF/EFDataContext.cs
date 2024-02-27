ebd180964cb6281a57bf673d4a7a2f57f8e89ff5
Reza7476 <rdehghani.akorn@gmail.com>
2024-02-27 10:37:41 +0330

    created project's infrastructer

---
+++ DoctorAppointment.Persistence.EF/EFDataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DoctorAppointment.Persistence.EF;
+public class EFDataContext : DbContext
+{
+
+    public EFDataContext (string connectionString):
+        this (new DbContextOptionsBuilder().UseSqlServer(connectionString).Options)
+    { }
+    public EFDataContext( DbContextOptions options):base(options)
+    { }
+        
+   
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(EFDataContext).Assembly);
+
+    }
+
+
+
+
+}
